28464c4d09c314ef984696750a062fe817e86b3f
ricardo03silva <rnsilva@DEV48.intranet.digitalsign.pt>
2023-03-15 12:01:46 +0000

    Finished Chapter 1 - Part2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -114,16 +114,157 @@
 // Code Challenge #02
 //**********************************
 //Data 1
-let markHeight = 1.69;
-let markMass = 78;
-let johnHeight = 1.95;
-let johnMass = 92;
+// let markHeight = 1.69;
+// let markMass = 78;
+// let johnHeight = 1.95;
+// let johnMass = 92;
 
-let markBMI = (markMass / markHeight **2);
-let johnBMI = (johnMass / johnHeight **2);
+// let markBMI = (markMass / markHeight **2);
+// let johnBMI = (johnMass / johnHeight **2);
 
-if (markBMI > johnBMI) {
-    console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})`);
-} else {
-    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})`);
-}
+// if (markBMI > johnBMI) {
+//     console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})`);
+// } else {
+//     console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})`);
+// }
+
+
+//**********************************
+// Type Conversion and Coercion
+//**********************************
+// //conversion
+// const inputYear = "1991";
+// console.log(inputYear + 18); //this creates the string 199118
+// console.log(Number(inputYear), inputYear); //1991 '1991'
+// console.log(Number(inputYear) + 18); //2009
+// console.log(String(19)); //19 as string
+
+// //coercion: auto conversions from JS
+// console.log('I am '+ 23 + ' years old'); //converst 23 to a string automatically
+// console.log('23' - '10' - 3); //10, JS converts strings to numbers but not numbers to strings
+
+// let n = '1' + 1; //'11'
+// --n; //'10'
+// console.log(n); //10
+
+
+//**********************************
+// Truty and Falsy values
+//**********************************
+//5 falsy values: 0, '', undefined, null, NaN
+// console.log(Boolean(0));
+// console.log(Boolean(''));
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+
+//0 and 1 are like false and true in conditions
+
+
+//**********************************
+// Equality operators
+//**********************************
+// const age = 18;
+// //with 3= the types need to be equal
+// //always use ===
+// if (age === '18') {
+//     console.log('True');
+// } else {
+//     console.log('False');
+// }
+
+// if (age == 18) {
+//     console.log('True');
+// } else {
+//     console.log('False');
+// }
+
+// let fav = prompt("Whats you fav number?");
+// fav = Number(prompt("Whats you fav number?"));
+// console.log(fav); //comes as a string
+// if (fav === 10) {
+//     console.log('same type');
+// } else {
+//     console.log('not same type');
+// }
+
+
+//**********************************
+// Boolean
+//**********************************
+// let hasLicense = true;
+// let hasVision = true;
+// console.log(hasLicense && hasVision); //true true - true
+// hasVision = false;
+// console.log(hasLicense && hasVision); //true false - false
+// console.log(hasLicense || hasVision); // - true
+// console.log(!hasLicense || hasVision); //false false - false
+
+
+
+//**********************************
+// Code Challenge #03
+//**********************************
+// let dolScore1 = 97;
+// let dolScore2 = 112;
+// let dolScore3 = 101;
+// let kolScore1 = 109;
+// let kolScore2 = 95;
+// let kolScore3 = 106;
+
+// let dolAverage = (dolScore1 + dolScore2 + dolScore3) / 3;
+// console.log(dolAverage);
+// let kolAverage = (kolScore1 + kolScore2 + kolScore3) / 3;
+// console.log(kolAverage);
+
+// if (dolAverage === kolAverage && dolAverage >= 100 && kolAverage >= 100) {
+//     console.log('Draw');
+// } else if (dolAverage > kolAverage && dolAverage >= 100) {
+//     console.log('Dolphins');
+// } else if (kolAverage >= 100) {
+//     console.log('Koalas');
+// } else {
+//     console.log('No Minimum Score');
+// }
+
+
+//**********************************
+// Switch Case
+//**********************************
+// const day = "Thursday"
+// switch (day) {
+//     case "Monday":
+//         console.log('lets code');
+//         break;
+//     case "Tuesday":
+//         console.log('lets watch a movie');
+//         break;
+//     case "Wednesday":
+//         console.log('lets play games');
+//         break;
+//     case "Thursday":
+//     case "Friday":
+//         console.log('go to the beach');
+//         break;    
+//     default:
+//         console.log('not a valid day');
+//         break;
+// }
+
+
+//**********************************
+// Ternary Operator
+//**********************************
+// const age = 23;
+// age >= 18 ? console.log('OK1') : console.log('OK2');
+
+// const drink = age >= 18 ? 'wine' : 'water';
+// console.log(drink);
+
+
+//**********************************
+// Code Challenge #04
+//**********************************
+let bill = 430;
+let tip = bill >= 50 && bill <= 300 ? bill*0.15 : bill*0.2;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill+tip}`);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -90,15 +90,240 @@ Code Challenge 01
 /****************
 Arrays
 ****************/
-const friends = ['Michael', 'Steven', 'Peter'];
-console.log(friends);
+// const friends = ['Michael', 'Steven', 'Peter'];
+// console.log(friends);
 
-//other way of creating arrays
-const years = new Array(1991,1992,2001,2023);
+// //other way of creating arrays
+// const y = new Array(1991,1992,2001,2020);
 
-console.log(friends.length);
-console.log(friends[friends.length - 1]); //last element
-console.log(friends[2]); //last element also
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]); //last element
+// console.log(friends[2]); //last element also
 
-friends[2] = 'Jay';
-console.log(friends);
+// friends[2] = 'Jay';
+// console.log(friends);
+
+// const firstName = 'Ricardo'
+// const ricardo = [firstName, 'Silva', 2023 - 2001, 'student',friends]
+// console.log(ricardo);
+
+// //Exercise
+// const calcAge = (birthYear) => 2023 - birthYear;
+// const years = [1990,1967,2001,2010];
+
+// const age1 = calcAge(years[years.length - 1]);
+// console.log(age1);
+// let ageCalculated = years.map(calcAge);
+// console.log(ageCalculated);
+
+// const friends = ['Michael', 'Steven', 'Peter'];
+// friends.push('Jay'); //add in the end
+// console.log(friends);
+
+// friends.unshift('John'); //add in the beginning
+// console.log(friends);
+
+// friends.pop() //removes the last element
+// console.log(friends);
+// friends.shift(); //removes the first element
+// console.log(friends);
+
+// console.log(friends.indexOf('Michael')); //returns index of element
+// console.log(friends.indexOf('Bob')); //returns -1 when element doesn't exist
+// console.log(friends.includes('Michael')); //returns true or false
+
+
+/****************
+Code Challenge 02
+****************/
+// const bills = [125,555,44];
+// const tips = [];
+// const total = [];
+
+// const calcTip = (bill) => {
+//     if (bill >= 50 && bill <= 300) {
+//         tips.push(bill*0.15);
+//         total.push(bill + tips[tips.length - 1]);
+//     } else {
+//         tips.push(bill*0.2);
+//         total.push(bill + tips[tips.length - 1]);
+//     }
+// }
+
+// bills.map(calcTip);
+// console.log(bills, tips, total);
+
+
+
+/****************
+Objects
+****************/
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Smith',
+//     age: 2023-1991,
+//     job: 'student',
+//     friends: ['Michael', 'Peter', 'Steven']
+// };
+// console.log(jonas);
+
+// console.log(jonas.lastName);
+// console.log(jonas['lastName']);
+
+// const nameKey = 'Name';
+// console.log(jonas['first' + nameKey]);
+
+// // const answer = prompt('What you want to know?');
+// // console.log(jonas[answer]);
+
+// jonas.location = 'Portugal'
+// jonas.twitter = '@username'
+// console.log(jonas);
+
+// console.log(`${jonas.firstName} has ${jonas.friends.length} friends and his best friend is ${jonas.friends[0]}`);
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Smith',
+//     birthYear: 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven'],
+//     hasDriversLicense: false,
+    
+//     calcAge: function() {
+//         this.age = 2037 - this.birthYear;
+//         return this.age;
+//     },
+//     getSummary: function() {
+//         return `${this.firstName} is a ${this.calcAge()} years old ${this.job} and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license`
+//     }
+// };
+
+// console.log(jonas.getSummary());
+
+
+/****************
+Code Challenge 03
+****************/
+// const mark = {
+//     firstName: 'Mark',
+//     lastName: 'Miler',
+//     mass: 78,
+//     height: 1.69,
+//     calcBMI: function() {
+//         this.bmi = this.mass / (this.height**2);
+//         return this.bmi;
+//     }
+// }
+// const john = {
+//     firstName: 'John',
+//     lastName: 'Smith',
+//     mass: 92,
+//     height: 1.95,
+//     calcBMI: function() {
+//         this.bmi = this.mass / (this.height**2);
+//         return this.bmi;
+//     }
+// }
+
+// mark.calcBMI();
+// john.calcBMI();
+
+// if (john.bmi > mark.bmi) {
+//     console.log(`John's BMI (${john.bmi}) is higher than Mark's (${mark.bmi})`);
+// } else {
+//     console.log(`Mark's BMI (${mark.bmi}) is higher than John's (${john.bmi})`);
+// }
+
+
+/****************
+For Loop
+****************/
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Repetition number ${i}`);
+// }
+
+// const ricardo = [
+//     'Ricardo', 
+//     'Silva', 
+//     2023 - 2001,
+//     'student',
+//     ['Michael', 'Peter', 'John']
+// ];
+
+// const types = [];
+
+// for (let i = 0; i < ricardo.length; i++) {
+//     types.push(typeof ricardo[i]);
+// }
+// console.log(types);
+
+// //only strings - skip other types
+// for (let i = 0; i < ricardo.length; i++) {
+//     if (typeof ricardo[i] !== 'string') continue;
+//     console.log(typeof ricardo[i]);
+// }
+
+// //break loop when number is find
+// for (let i = 0; i < ricardo.length; i++) {
+//     if (typeof ricardo[i] === 'number') break;
+//     console.log(typeof ricardo[i]);
+// }
+
+// //backwards loop
+// for (let i = ricardo.length - 1; i >= 0; i--) {
+//     console.log(i, ricardo[i]);
+// }
+
+// for (let i = 1; i <= 2; i++) {
+//     console.log(`----- Exercise number ${i}`);
+//     for (let x = 1; x <= 3; x++) {
+//         console.log(`Repetition number ${x}`);
+//     }
+// }
+
+// //while loop
+// let rep = 1;
+// while (rep <= 5) {
+//     console.log(`Repetition number ${rep}`);
+//     rep++;
+// }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// while (dice !== 6) {
+//     console.log(`Rolled a dice ${dice}`);
+//     dice = Math.trunc(Math.random() * 6) + 1;
+//     if (dice === 6) console.log('Loop is ending');
+// }
+
+
+/****************
+Code Challenge 04
+****************/
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = (bill) => {
+    if (bill >= 50 && bill <= 300) {
+        return bill*0.15;
+    } else {
+        return bill*0.2;
+    }
+}
+
+for (let i = 0; i < bills.length; i++) {
+    const element = bills[i];
+    tips.push(calcTip(element));
+    totals.push(tips[i]+element);
+}
+
+const calcAverage = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum/arr.length;
+}
+
+console.log(bills, tips, totals);
+console.log(calcAverage(totals));
