df3b8caad0f771d104655ed7c00e796a0d0cf4e2
ricardo03silva <rnsilva@DEV48.intranet.digitalsign.pt>
2023-03-16 12:58:04 +0000

    Modal not closing - FIX PROBLEM

--- 06-Modal/final/script.js
+++ 06-Modal/final/script.js
@@ -15,8 +15,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
+for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
+}
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('started');
+const modal = document.querySelector('.modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const openModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+btnOpenModal.forEach((element) => {
+    addEventListener('click', openModal);
+});
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
