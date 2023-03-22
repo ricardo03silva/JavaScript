093da95cd88f26b01fd45d72b1f63f82170fe16f
ricardo03silva <rnsilva@DEV48.intranet.digitalsign.pt>
2023-03-22 13:00:28 +0000

    started

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight); //is passed by value
+console.log(jonas); //is passed by refence (it's a object)
+//checkIn changes 'jonas' value but not flight value
+
+const flightNum = flight; //copies the value
+const passenger = jonas; //copies the reference
