258e6a68572f9863efa2f4e627d3e15f503b3606
ricardo03silva <rnsilva@DEV48.intranet.digitalsign.pt>
2023-03-28 12:59:51 +0100

    Lesson 265 DONE, 2 MISSING

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -18,6 +18,7 @@ const renderCountry = (data, className = '') => {
               </div>
             </article>
         `;
+    countriesContainer.style.opacity = 1;
     countriesContainer.insertAdjacentHTML('beforeend', html);
 };
 
@@ -159,3 +160,174 @@ const whereAmI2 = (lat, lng) => {
         .catch((err) => console.error(err));
 };
 // whereAmI2(-33.933, 18.474);
+
+// //Event Loop
+// console.log('Test start');
+// setTimeout(() => console.log('0 sec timer'), 0);
+// Promise.resolve('Resolved promise 1').then((res) => console.log(res));
+// console.log('Test end');
+/*OUTPUT
+--estes 2 console.log correm fora da callback queue e executam primeiro
+LINE 164: Test start
+LINE 167: Test end
+--depois vem o seguinte:
+LINE 166: Resolved promise 1 //promisse gera uma micro task que tem prioridade em relacao à callback queue por isso vem primeiro que '0 sec timer'
+LINE 165: 0 sec timer //vai para a callback queue
+*/
+
+// //Building a Promisse
+// //assegurar que codigo assincrono corre de forma ordenada
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//     console.log('Lottery Happening');
+//     setTimeout(() => {
+//         if (Math.random() >= 0.5) {
+//             resolve('You Win');
+//         } else {
+//             reject(new Error('You Lost'));
+//         }
+//     }, 2000);
+// });
+
+// lotteryPromise
+//     .then((response) => {
+//         console.log(response);
+//     })
+//     .catch((err) => console.error(err));
+
+const wait = function (seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+// wait(2)
+//     .then(() => {
+//         console.log('Waited for 2 seconds');
+//         return wait(1);
+//     })
+//     .then(() => {
+//         console.log('Waited for 1 second');
+//     });
+
+const getPosition = () => {
+    return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+};
+
+const whereAmI3 = () => {
+    getPosition()
+        .then((pos) => {
+            const { latitude: lat, longitude: lng } = pos.coords;
+            return fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`);
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`(${response.status}): ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then((response) => {
+            console.log(`You are in ${response.city}, ${response.countryName}`);
+            getData(response.countryName);
+        })
+        .catch((err) => console.error(err));
+};
+// whereAmI3();
+
+// //Chalenge 02
+// const imagesContainer = document.querySelector('.images');
+
+// const createImage = (imgPath) => {
+//     return new Promise(function (resolve, reject) {
+//         const img = document.createElement('img');
+//         img.src = imgPath;
+
+//         img.addEventListener('load', () => {
+//             imagesContainer.append(img);
+//             resolve(img);
+//         });
+
+//         img.addEventListener('error', () => {
+//             reject(new Error('Img failed loading'));
+//         });
+//     });
+// };
+
+// let current;
+
+// createImage('img/img-1.jpg')
+//     .then((response) => {
+//         current = response;
+//         console.log('Img1 loaded');
+//         return wait(5);
+//     })
+//     .then(() => {
+//         current.style.display = 'none';
+//         return createImage('img/img-2.jpg');
+//     })
+//     .then((response) => {
+//         current = response;
+//         return wait(5);
+//     })
+//     .then(() => {
+//         current.style.display = 'none';
+//     })
+//     .catch((err) => {
+//         console.error(err);
+//     });
+
+//the same as: fetch(`https://restcountries.com/v2/name/${country}`).then(...)
+//Async and Await and try...catch
+//async simplifies promises consuming
+const newLocation = async () => {
+    try {
+        //geolocation
+        const pos = await getPosition();
+        const { latitude: lat, longitude: lng } = pos.coords;
+
+        //reverse geocoding
+        const resGeo = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`);
+        if (!resGeo.ok) throw new Error('Error in reverse geocoding');
+        const dataGeo = await resGeo.json();
+
+        //get country data
+        const res = await fetch(`https://restcountries.com/v2/name/${dataGeo.countryName}`);
+        if (!res.ok) throw new Error('Not able to get country data');
+        const resJSON = await res.json();
+        renderCountry(resJSON[0]);
+
+        return `You are in ${dataGeo.city}, ${dataGeo.countryName}`;
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// (async () => {
+//     console.log('1');
+//     try {
+//         const city = await newLocation();
+//         console.log(`2 ${city}`);
+//     } catch (err) {
+//         console.error(err);
+//     }
+//     console.log('3');
+// })();
+
+const getCapitals = async (c1, c2, c3) => {
+    try {
+        //one at a time
+        // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+        // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+        // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+        // console.log([data1.capital, data2.capital, data3.capital]);
+
+        //all at the same time: check network tab in dev tools
+        const data = await Promise.all([getJSON(`https://restcountries.com/v2/name/${c1}`), getJSON(`https://restcountries.com/v2/name/${c2}`), getJSON(`https://restcountries.com/v2/name/${c3}`)]);
+
+        console.log(data.map((el) => el[0].capital));
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+getCapitals('portugal', 'spain', 'italy');
